996205d2ed94d0296207b1f6cd851ecba41d5cd8
AbdoFoda <abdofoda@stud.fci-cu.edu.eg>
2026-06-14 22:14:17 +0200

    increase test coverage for the ElevationEstimator for the unhappy pathes

--- WeatherCore/Tests/WeatherCoreTests/Support/ElevationEstimatorTests.swift
+++ WeatherCore/Tests/WeatherCoreTests/Support/ElevationEstimatorTests.swift
@@ -43,4 +43,100 @@ final class ElevationEstimatorTests: XCTestCase {
             )
         )
     }
+
+    func test_metersAboveSeaLevel_returnsNilWhenGroundPressureIsZero() {
+        XCTAssertNil(
+            ElevationEstimator.metersAboveSeaLevel(
+                seaLevelHPa: 1013,
+                groundLevelHPa: 0,
+                fallbackSeaLevelHPa: nil
+            )
+        )
+    }
+
+    func test_metersAboveSeaLevel_returnsNilWhenGroundPressureIsNegative() {
+        XCTAssertNil(
+            ElevationEstimator.metersAboveSeaLevel(
+                seaLevelHPa: 1013,
+                groundLevelHPa: -1,
+                fallbackSeaLevelHPa: nil
+            )
+        )
+    }
+
+    func test_metersAboveSeaLevel_returnsNilWhenGroundExceedsSeaLevel() {
+        XCTAssertNil(
+            ElevationEstimator.metersAboveSeaLevel(
+                seaLevelHPa: 1000,
+                groundLevelHPa: 1005,
+                fallbackSeaLevelHPa: nil
+            )
+        )
+    }
+
+    func test_metersAboveSeaLevel_returnsNilWithoutSeaOrFallbackPressure() {
+        XCTAssertNil(
+            ElevationEstimator.metersAboveSeaLevel(
+                seaLevelHPa: nil,
+                groundLevelHPa: 1009,
+                fallbackSeaLevelHPa: nil
+            )
+        )
+    }
+
+    func test_metersAboveSeaLevel_returnsNilWhenSeaLevelIsZero() {
+        XCTAssertNil(
+            ElevationEstimator.metersAboveSeaLevel(
+                seaLevelHPa: 0,
+                groundLevelHPa: 1009,
+                fallbackSeaLevelHPa: nil
+            )
+        )
+    }
+
+    func test_metersAboveSeaLevel_returnsNilWhenFallbackPressureIsZero() {
+        XCTAssertNil(
+            ElevationEstimator.metersAboveSeaLevel(
+                seaLevelHPa: nil,
+                groundLevelHPa: 1009,
+                fallbackSeaLevelHPa: 0
+            )
+        )
+    }
+
+    func test_metersAboveSeaLevel_fromMainWeather_usesSeaAndGroundFields() {
+        let main = MainWeather(
+            temp: 20,
+            feelsLike: 20,
+            tempMin: 18,
+            tempMax: 22,
+            pressure: 1013,
+            seaLevel: 1013,
+            grndLevel: 1009,
+            humidity: 50
+        )
+
+        let elevation = ElevationEstimator.metersAboveSeaLevel(from: main)
+
+        XCTAssertNotNil(elevation)
+        XCTAssertEqual(elevation ?? -1, 33, accuracy: 2)
+    }
+
+    func test_metersAboveSeaLevel_fromMainWeather_fallsBackToPressureWhenSeaLevelMissing() {
+        let main = MainWeather(
+            temp: 20,
+            feelsLike: 20,
+            tempMin: 18,
+            tempMax: 22,
+            pressure: 1013,
+            seaLevel: nil,
+            grndLevel: 1009,
+            humidity: 50
+        )
+
+        let elevation = ElevationEstimator.metersAboveSeaLevel(from: main)
+
+        XCTAssertNotNil(elevation)
+        XCTAssertEqual(elevation ?? -1, 33, accuracy: 2)
+    }
 }
